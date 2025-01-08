20f97552b4c2b7999cb9a30b3be3476ce1adb833
Steve Váradi <varadiista.1@gmail.com>
2025-01-08 02:03:01 +0100

    set up immich

---
+++ apps/immich.tf
@@ -0,0 +1,11 @@
+# resource "helm_release" "immich" {
+#   name       = "immich"
+#   namespace  = kubernetes_namespace.apps.metadata[0].name
+#   chart      = "immich"
+#   version    = "2.1.0"
+#   repository = "https://immich-app.github.io/immich-charts"
+#   values     = [file("${path.module}/immich-values.yaml")]
+#   timeout    = 600
+#   depends_on = [kubernetes_namespace.apps]
+# }
+
