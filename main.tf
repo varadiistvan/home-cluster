5b84e8d29ebc2c13e43d0a0c0ca7a0ba35562c6e
Steve Váradi <varadiista.1@gmail.com>
2025-09-15 18:09:49 +0200

    endless-wiki with external ollama, intel gpu

--- main.tf
+++ main.tf
@@ -20,6 +20,10 @@ terraform {
       source  = "hashicorp/time"
       version = "0.13.1"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.5.0"
+    }
   }
 
   backend "s3" {
