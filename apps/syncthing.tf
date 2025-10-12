01a3ca58b582ccbf3eacd01b7dfd2937da25fa0f
Steve Váradi <varadiista.1@gmail.com>
2025-10-12 18:13:43 +0200

    Successfully deploy syncthing

--- apps/syncthing.tf
+++ apps/syncthing.tf
@@ -1,11 +1,33 @@
+locals {
+  syncthing_ip = "192.168.0.154"
+}
+
+resource "kubectl_manifest" "syncthing_ip_pool" {
+  yaml_body = <<-YAML
+    apiVersion: cilium.io/v2
+    kind: CiliumLoadBalancerIPPool
+    metadata:
+      name: vip-syncthing
+    spec:
+      blocks:
+        - start: ${local.syncthing_ip}
+          stop:  ${local.syncthing_ip}
+  YAML
+}
+
+
 resource "helm_release" "syncthing" {
   name       = "syncthing"
   namespace  = kubernetes_namespace.apps.id
   chart      = "syncthing"
-  version    = "22.6.0"
-  repository = "oci://oci.trueforge.org/truecharts"
+  version    = "0.1.0"
+  repository = "oci://harbor.stevevaradi.me/stevevaradi"
   values     = [file("${path.module}/values/syncthing-values.yaml")]
   timeout    = 600
+
+  repository_password = var.home_registry_password
+  repository_username = "stevev"
+
 
   lifecycle {
     ignore_changes = [metadata]
