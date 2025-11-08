eff5e06089d4c92349056bec843aa758de4db608
Steve Váradi <varadiista.1@gmail.com>
2025-11-08 11:29:19 +0100

    things
    Signed-off-by: Steve Váradi <varadiista.1@gmail.com>

--- apps/jellyfin.tf
+++ apps/jellyfin.tf
@@ -2,7 +2,7 @@ resource "helm_release" "jellyfin" {
   name       = "jellyfin"
   namespace  = kubernetes_namespace.apps.metadata[0].name
   chart      = "jellyfin"
-  version    = "2.3.0"
+  version    = "2.5.0"
   repository = "https://jellyfin.github.io/jellyfin-helm"
   values     = [file("${path.module}/values/jellyfin-values.yaml")]
   timeout    = 600
