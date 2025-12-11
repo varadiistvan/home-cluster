30da18ab01db9b6ce646849e858da5da8341b5c4
Steve Váradi <varadiista.1@gmail.com>
2025-12-11 14:44:35 +0100

    Stuff
    Signed-off-by: Steve Váradi <varadiista.1@gmail.com>

--- apps/jellyfin.tf
+++ apps/jellyfin.tf
@@ -13,3 +13,19 @@ resource "helm_release" "jellyfin" {
   }
 }
 
+
+resource "helm_release" "jellyseerr" {
+  name       = "jellyseer"
+  namespace  = kubernetes_namespace.apps.metadata[0].name
+  chart      = "jellyseerr-chart"
+  version    = "2.7.0"
+  repository = "oci://ghcr.io/fallenbagel/jellyseerr"
+  values     = [file("${path.module}/values/jellyseer-values.yaml")]
+  timeout    = 300
+  depends_on = [kubernetes_namespace.apps]
+
+  lifecycle {
+    ignore_changes = [metadata]
+  }
+}
+
